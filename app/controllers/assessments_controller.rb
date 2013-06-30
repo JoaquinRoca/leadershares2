e345c7bcb861cf79aed0c067ca3f14eb400068a6
Joaquin Roca <me@joaquinroca.com>
2013-06-30 18:48:05 -0400

    Re-order assessment controller actions alphabetically

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,41 +1,4 @@
 class AssessmentsController < ApplicationController
-  # GET /assessments
-  # GET /assessments.json
-  def index
-    @assessments = Assessment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @assessments }
-    end
-  end
-
-  # GET /assessments/1
-  # GET /assessments/1.json
-  def show
-    @assessment = Assessment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @assessment }
-    end
-  end
-
-  # GET /assessments/new
-  # GET /assessments/new.json
-  def new
-    @assessment = Assessment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @assessment }
-    end
-  end
-
-  # GET /assessments/1/edit
-  def edit
-    @assessment = Assessment.find(params[:id])
-  end
 
   # POST /assessments
   # POST /assessments.json
@@ -50,6 +13,56 @@ class AssessmentsController < ApplicationController
         format.html { render action: "new" }
         format.json { render json: @assessment.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  # DELETE /assessments/1
+  # DELETE /assessments/1.json
+  def destroy
+    @assessment = Assessment.find(params[:id])
+    @assessment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to assessments_url }
+      format.json { head :no_content }
+    end
+  end
+
+  # GET /assessments/1/edit
+  def edit
+    @assessment = Assessment.find(params[:id])
+  end
+
+  # GET /assessments
+  # GET /assessments.json
+  def index
+    @assessments = Assessment.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @assessments }
+    end
+  end
+
+  # GET /assessments/new
+  # GET /assessments/new.json
+  def new
+    @assessment = Assessment.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @assessment }
+    end
+  end
+
+  # GET /assessments/1
+  # GET /assessments/1.json
+  def show
+    @assessment = Assessment.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @assessment }
     end
   end
 
@@ -69,15 +82,5 @@ class AssessmentsController < ApplicationController
     end
   end
 
-  # DELETE /assessments/1
-  # DELETE /assessments/1.json
-  def destroy
-    @assessment = Assessment.find(params[:id])
-    @assessment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to assessments_url }
-      format.json { head :no_content }
-    end
-  end
 end
