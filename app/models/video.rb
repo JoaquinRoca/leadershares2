5bd095199173d68ba36fb80259e6dd6ebeb305a6
Joaquin Roca <me@joaquinroca.com>
2013-08-02 18:01:59 -0400

    Added the ability to select the competencies for a video when adding or editing a video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
-  attr_accessible :interviewee, :title, :url
+  attr_accessible :competency_ids, :interviewee, :title, :url
   has_and_belongs_to_many :competencies
 
   validates :title, presence: true
